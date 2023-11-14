fc8708be4ca392726fb850106f0c32515ed3a56f
sontaya <sontaya@aspiron.co.th>
2023-11-14 18:59:09 +0700

    dsfdfgdfgdgs

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-const express = require('express');
-const ws = require('ws');
+const express = require("express");
+const ws = require("ws");
 
 const app = express();
 
 // Set up a headless websocket server that prints any
 // events that come in.
-const wsServer = new ws.Server({ noServer: true });
-wsServer.on('connection', socket => {
-    console.log('connected ');
-  socket.on('message', message =>{
-    console.log(message+'');
-    socket.send('message+ testconn ')
-});
+const wsServer = new ws.Server({ port: 3306 });
+  console.log(wsServer);
+wsServer.on("connection", (socket) => {
+  console.log("connected ");
 
-// socket.on()
-})
+
+
+   
+
+  socket.on("message", (message) => {
+    console.log(message+'');
+    socket.send("message+ testconn ");
+  });
+
+  // socket.on()
+});
 // wsServer.on('message', sc =>{
 //     console.log({sc});
 // sc.send('test')
@@ -24,9 +30,9 @@ wsServer.on('connection', socket => {
 // the same ws upgrade process described here:
 // https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
 const server = app.listen(3005);
-server.on('upgrade', (request, socket, head) => {
-  wsServer.handleUpgrade(request, socket, head, socket => {
-    wsServer.emit('connection', socket, request);
+server.on("upgrade", (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, (socket) => {
+    wsServer.emit("connection", socket, request);
   });
 });
 // app.listen(3005);
