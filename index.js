aefe9a5cf89fa1eb9871ce2f64e47d5abd77b3ba
sontaya <sontaya@aspiron.co.th>
2023-11-14 19:58:05 +0700

    update

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
-const express = require("express");
-const ws = require("ws");
+// const express = require("express");
+// const ws = require("ws");
+// const app = express();
 
-const app = express();
+// const wsServer = new ws.Server({noServer:true});
+// wsServer.on("connection", (socket) => {
+//   console.log("connected ");
 
-// Set up a headless websocket server that prints any
-// events that come in.
-
-const wsServer = new ws.Server({noServer:true});
-  // console.log(wsServer);/
-wsServer.on("connection", (socket) => {
-  console.log("connected ");
-
-  socket.on("message",(message) => {
-    console.log(message +'');
-    for (let i = 0; i <3; i++) {
-      socket.send("okk")
-    }
-  });
-})
-
-
-  // socket.on()
-
-// wsServer.on('message', sc =>{
-//     console.log({sc});
-// sc.send('test')
+//   socket.on("message",(message) => {
+//     console.log(message +'');
+//     for (let i = 0; i <3; i++) {
+//       socket.send("okk")
+//     }
+//   });
 // })
+// const server = app.listen(6000);
+// server.on("upgrade", (request, socket, head) => {
+//   wsServer.handleUpgrade(request, socket, head, (socket) => {
+//     wsServer.emit("connection", socket, request);
+//   });
+// });
 
-// `server` is a vanilla Node.js HTTP server, so use
-// the same ws upgrade process described here:
-// https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
-const server = app.listen(6000);
-server.on("upgrade", (request, socket, head) => {
-  wsServer.handleUpgrade(request, socket, head, (socket) => {
-    wsServer.emit("connection", socket, request);
+const WebSocket = require('ws');
+const server = new WebSocket.Server({
+  port:6000
+});
+
+let sockets = [];
+server.on('connection', function(socket) {
+  sockets.push(socket);
+
+  // When you receive a message, send that message to every socket.
+  socket.on('message', function(msg) {
+    msg+=''
+    console.debug({msg})
+
+    sockets.forEach(s => s.send(msg));
+  });
+
+  // When a socket closes, or disconnects, remove it from the array.
+  socket.on('close', function() {
+    sockets = sockets.filter(s => s !== socket);
   });
 });
-// app.listen(3005);
+
+
