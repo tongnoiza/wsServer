672b501cc621dcfe0b6c9c98df386718c67130a8
sontaya <sontaya@aspiron.co.th>
2023-11-14 19:27:35 +0700

    update

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const app = express();
 
 // Set up a headless websocket server that prints any
 // events that come in.
-const wsServer = new ws.Server({ port: 6000 });
-  console.log(wsServer);
+const wsServer = new ws.Server({noServer:true});
+  // console.log(wsServer);/
 wsServer.on("connection", (socket) => {
   console.log("connected ");
 
   socket.on("message",async (message) => {
-    console.log(message+'');
-   await socket.send("message+ testconn ");
+    console.log(message +'');
+    for (let i = 0; i <10; i++) {
+      await socket.send("message+ testconn ");
+    }
+  
   });
 
   // socket.on()
@@ -25,10 +28,10 @@ wsServer.on("connection", (socket) => {
 // `server` is a vanilla Node.js HTTP server, so use
 // the same ws upgrade process described here:
 // https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
-// const server = app.listen(6000);
-// server.on("upgrade", (request, socket, head) => {
-//   wsServer.handleUpgrade(request, socket, head, (socket) => {
-//     wsServer.emit("connection", socket, request);
-//   });
-// });
+const server = app.listen(6000);
+server.on("upgrade", (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, (socket) => {
+    wsServer.emit("connection", socket, request);
+  });
+});
 // app.listen(3005);
