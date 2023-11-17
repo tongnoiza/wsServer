7d489429fcc7abb6273a663dc5f428a02fac383e
sontaya <sontaya@aspiron.co.th>
2023-11-17 11:56:10 +0700

    ws

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
-// const express = require("express");
-// const ws = require("ws");
-// const app = express();
+// const WebSocket = require('ws');
+// const server = new WebSocket.Server({
+//   port:6000
+// });
 
-// const wsServer = new ws.Server({noServer:true});
-// wsServer.on("connection", (socket) => {
-//   console.log("connected ");
-
-//   socket.on("message",(message) => {
-//     console.log(message +'');
-//     for (let i = 0; i <3; i++) {
-//       socket.send("okk")
-//     }
+// let sockets = [];
+// server.on('connection', function(socket) {
+//   sockets.push(socket);
+//   // When you receive a message, send that message to every socket.
+//   socket.on('message', function(msg) {
+//     msg+=''
+//     console.debug({msg})
+//     sockets.forEach(s => s.send(msg));
 //   });
-// })
-// const server = app.listen(6000);
-// server.on("upgrade", (request, socket, head) => {
-//   wsServer.handleUpgrade(request, socket, head, (socket) => {
-//     wsServer.emit("connection", socket, request);
+
+//   // When a socket closes, or disconnects, remove it from the array.
+//   socket.on('close', function() {
+//     sockets = sockets.filter(s => s !== socket);
 //   });
 // });
 
-const WebSocket = require('ws');
-const server = new WebSocket.Server({
-  port:6000
-});
 
-let sockets = [];
-server.on('connection', function(socket) {
-  sockets.push(socket);
+var express = require('express')
+var expressWs = require('express-ws')
 
-  // When you receive a message, send that message to every socket.
-  socket.on('message', function(msg) {
-    msg+=''
-    console.debug({msg})
+var app = express()
+expressWs(app)
 
-    sockets.forEach(s => s.send(msg));
-  });
+app.ws('/echo', (ws, req) => {
 
-  // When a socket closes, or disconnects, remove it from the array.
-  socket.on('close', function() {
-    sockets = sockets.filter(s => s !== socket);
-  });
-});
+    ws.on('connection', function (connection) {
+        //...
+    })
 
+    ws.on('close', function () {
+        //...
+    })
+})
 
+app.use(express.static('public'))
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
