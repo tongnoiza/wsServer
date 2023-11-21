6f82f958eea91c6f31b048bde2593670fdd6ea02
sontaya <sontaya@aspiron.co.th>
2023-11-21 12:34:06 +0700

    update code

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-// const WebSocket = require('ws');
-// const server = new WebSocket.Server({
-//   port:6000
-// });
+const WebSocket = require('ws');
+const server = new WebSocket.Server({
+  port:80
+});
 
-// let sockets = [];
-// server.on('connection', function(socket) {
-//   sockets.push(socket);
-//   // When you receive a message, send that message to every socket.
-//   socket.on('message', function(msg) {
-//     msg+=''
-//     console.debug({msg})
-//     sockets.forEach(s => s.send(msg));
-//   });
-
-//   // When a socket closes, or disconnects, remove it from the array.
-//   socket.on('close', function() {
-//     sockets = sockets.filter(s => s !== socket);
-//   });
-// });
-
-import { WebSocketServer } from 'ws';
-let port = process.env.port | 5000
-console.debug(`running at port ${port}`)
-const wss = new WebSocketServer({ port: port});
-
-wss.on('connection', function connection(ws) {
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
+let sockets = [];
+server.on('connection', function(socket) {
+  sockets.push(socket);
+  // When you receive a message, send that message to every socket.
+  socket.on('message', function(msg) {
+    msg+=''
+    console.debug({msg})
+    sockets.forEach(s => s.send(msg));
   });
 
-  ws.send('something');
+  // When a socket closes, or disconnects, remove it from the array.
+  socket.on('close', function() {
+    sockets = sockets.filter(s => s !== socket);
+  });
 });
+
+// import { WebSocketServer } from 'ws';
+// let port = process.env.port | 5000
+// // console.debug(`running at port ${port}`)
+// const wss = new WebSocketServer({ port: port});
+// console.log(process.env);
+// wss.on('connection', function connection(ws) {
+//   ws.on('message', function message(data) {
+//     console.log('received: %s', data);
+//   });
+
+//   ws.send('something');
+// });
