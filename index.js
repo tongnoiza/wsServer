d94133ed69ef1947c31eb9fc812b369d37043ac8
sontaya <sontaya@aspiron.co.th>
2023-11-21 13:19:49 +0700

    update code

--- index.js
+++ index.js
@@ -1,33 +1,54 @@
-const WebSocket = require('ws');
-const server = new WebSocket.Server({
-  port:80
-});
+// const WebSocket = require('ws');
+// const server = new WebSocket.Server({
+//   port:80,
 
-let sockets = [];
-server.on('connection', function(socket) {
-  sockets.push(socket);
-  // When you receive a message, send that message to every socket.
-  socket.on('message', function(msg) {
+// });
+
+// let sockets = [];
+// server.on('connection', function(socket) {
+//   sockets.push(socket);
+//   // When you receive a message, send that message to every socket.
+//   socket.on('message', function(msg) {
+//     msg+=''
+//     console.debug({msg})
+//     sockets.forEach(s => s.send(msg));
+//   });
+
+//   // When a socket closes, or disconnects, remove it from the array.
+//   socket.on('close', function() {
+//     sockets = sockets.filter(s => s !== socket);
+//   });
+// });
+
+import { createServer } from 'http';
+import { parse } from 'url';
+import { WebSocketServer } from 'ws';
+
+const server = createServer();
+const wss1 = new WebSocketServer({ noServer: true });
+
+wss1.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+    ws.on('message', function(msg) {
     msg+=''
     console.debug({msg})
     sockets.forEach(s => s.send(msg));
   });
 
-  // When a socket closes, or disconnects, remove it from the array.
-  socket.on('close', function() {
-    sockets = sockets.filter(s => s !== socket);
-  });
+  // ...
 });
 
-// import { WebSocketServer } from 'ws';
-// let port = process.env.port | 5000
-// // console.debug(`running at port ${port}`)
-// const wss = new WebSocketServer({ port: port});
-// console.log(process.env);
-// wss.on('connection', function connection(ws) {
-//   ws.on('message', function message(data) {
-//     console.log('received: %s', data);
-//   });
 
-//   ws.send('something');
-// });
+server.on('upgrade', function upgrade(request, socket, head) {
+  const { pathname } = parse(request.url);
+
+  if (pathname === '/') {
+    wss1.handleUpgrade(request, socket, head, function done(ws) {
+      wss1.emit('connection', ws, request);
+    });
+  }  else {
+    socket.destroy();
+  }
+});
+
+server.listen(8080);
