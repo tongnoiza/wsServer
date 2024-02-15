8e165a89e852c35010aeb7e3c153a158d53e4573
sontaya <sontaya@aspiron.co.th>
2024-02-15 13:27:03 +0700

    ws

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 
 import express from "express";
 import cors from "cors";
-
-import { WebSocketServer } from "ws";
+import WebSocket, { WebSocketServer } from 'ws';
+// import { WebSocketServer } from "ws";
 let app = express();
 app.use(cors());
 
 const wss = new WebSocketServer({ port: 443 });
+
 app.on("upgrade", (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, (ws) => {
     wss.emit("connection", ws, request);
   });
 });
+
 wss.on("connection", function connection(ws) {
   console.log("เชื่อมต่อ");
   ws.on("error", console.error);
-
-  ws.on("message", function message(data, isBinary) {
-    console.log("received: %s", data);
-    wss.clients.forEach((client)=> {
+  // ws.on("message", function message(data) {
+  //   console.log("received: %s", data);
+  //   ws.send("something " + data);
+  // });
+  ws.on('message', function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
         client.send(data, { binary: isBinary });
       }
     });
-    // ws.send("something " + data);
   });
 });
-
+// wss.on("message", function message(data) {
+//   console.log("received: %s", data);
+//   ws.send("something " + data);
+// });
 
