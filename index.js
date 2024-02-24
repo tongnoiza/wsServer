8fe0b58c5fb8ff41a54bc071a4eb80c48bb79ce5
sontaya <sontaya@aspiron.co.th>
2024-02-24 19:03:53 +0700

    ws

--- index.js
+++ index.js
@@ -1,58 +1,15 @@
-//npm install websocket
-var WebSocketServer = require('websocket').server;
-var http = require('http');
+import { WebSocketServer } from 'ws';
+import  Express  from 'express';
+import cors from 'cors'
+const app = Express()
 
-var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
-});
-server.listen(443, function() {
-    console.log((new Date()) + ' Server is listening on port 5000');
-});
+const wss = new WebSocketServer({ port: 443 });
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
 
-wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
-});
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
 
-function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
-}
-
-wsServer.on('request', function(request) {
-    console.log(request)
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
-    }
-    
-    var connection = request.accept(null, request.origin)
-    console.log((new Date()) + ' Connection accepted.');
-
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            //connection.sendUTF(message.utf8Data); this resend the reseived message, instead of it i will send a custom message. hello from nodejs
-            connection.sendUTF("Hello from node.js");
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
-    });
-
-
-
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
+  ws.send('something');
 });
