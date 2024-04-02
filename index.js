726b6504054a6d6062e02f4e8039e46ebccb6722
sontaya <sontaya@aspiron.co.th>
2024-04-02 09:59:59 +0700

    'update'

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
   ws.on('message', function message(data, isBinary) {
+    console.log('test mcu ',data);
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
+      
         client.send(data, { binary: isBinary });
       }
     });
